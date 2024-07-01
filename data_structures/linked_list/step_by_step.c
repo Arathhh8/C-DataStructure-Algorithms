c675e08b95ebd548c2be9ec971130e3e1862c808
Arath Grarcía <arathgg143@gmail.com>
2024-07-01 01:02:01 -0600

    todo: implement a function to search a node by the value given

--- data_structures/linked_list/step_by_step.c
+++ data_structures/linked_list/step_by_step.c
@@ -23,6 +23,10 @@ void insertAtEnd(ListNode** head, int value);
 // Implementa una función para borrar un nodo que contiene un valor específico.
 void deleteNode(ListNode** head, int value);
 
+// 5. Imprimir la lista
+// Implementa una funcion para imprimir todos los nodos de la lista
+void printList(ListNode* head);
+
 
 
 int main() {
@@ -34,14 +38,14 @@ int main() {
     insertAtEnd(&head, 3);
 
     // Imprime la lista
-    printf("Lista después de insertar 3 nodos: ");
+    printf("Lista despues de insertar 3 nodos: ");
     printList(head);
 
     // Elimina un nodo
     deleteNode(&head, 2);
 
     // Imprime la lista después de eliminar
-    printf("Lista después de eliminar el nodo con valor 2: ");
+    printf("Lista despues de eliminar el nodo con valor 2: ");
     printList(head);
 
     // Libera la memoria
@@ -56,7 +60,7 @@ ListNode* createNode(int value){
     ListNode* newNode = (ListNode*)malloc(sizeof(ListNode)); // Reserva memoria para el nuevo nodo
     if(newNode == NULL){
         printf("Memory allocation failed");
-        return;
+        return NULL;
     }
 
     newNode->value = value; // asigno el valor al nuevo nodo
@@ -72,6 +76,7 @@ void insertAtEnd(ListNode** head, int value){
     // Si la lista está vacía (*head es NULL), asigna el nuevo nodo a *head.
     if(*head == NULL){
         *head = newNode;
+        return;
     }
 
     current = *head;
@@ -85,24 +90,57 @@ void insertAtEnd(ListNode** head, int value){
 
 void deleteNode(ListNode** head, int value){
 
+    // Si la lista esta vacia, no realizar ninguna operacion
     if (*head == NULL) {
+        printf("La lista esta vacia");
         return;
     }
 
+    // creamos un puntero que mantenta el nodo actual y otro para mantener un enlace al nodo previo
     ListNode* current = *head;
     ListNode* previous = NULL;
 
+    // Se maneja el caso en el que el nodo a eliminar sea el primero
     if(current != NULL && current->value == value){
         *head = current->next;
         free(current);
         return;
     }
 
+    // Se recorre la lista para encontrar el nodo con el valor dado
     while(current != NULL && current->value != value){
         previous = current;
         current = current->next;
     }
 
-    
-    
+    // Si no se encuentra el valor
+    if (current == NULL) {
+        printf("Node was not found\n");
+        return;
+    }
+
+    // Eliminar el nodo
+    previous->next = current->next;
+    free(current);
+    printf("Nodo con valor %d eliminado.\n", value);
+}
+
+
+void printList(ListNode* head){
+
+    ListNode* current = head;
+
+    // Si la lista esta vacia, no realizar ninguna operacion
+    if (head == NULL) {
+        return;
+    }
+
+    while (current != NULL){
+        printf("%d -> ", current->value);
+        current = current->next;
+    }
+
+    printf("NULL");
+    printf("\n");
+
 }
