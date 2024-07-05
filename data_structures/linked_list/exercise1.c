997cd8c110fd1e016319254002dd3ce8e5aa857d
Arath Grarcía <arathgg143@gmail.com>
2024-07-05 01:53:23 -0600

    feat: my own linked list was implemented my_linked_list

---
+++ data_structures/linked_list/exercise1.c
@@ -0,0 +1,23 @@
+/*
+Problema: Gestión de una lista de tareas
+
+Diseña un programa en C que permita gestionar una lista de tareas usando una lista enlazada. Cada tarea estará representada por un nodo de la lista enlazada, que contendrá el nombre de la tarea y su estado (por ejemplo, completada o pendiente).
+
+Funcionalidades requeridas:
+
+Agregar una tarea: Permitir al usuario ingresar el nombre de una nueva tarea y agregarla al final de la lista enlazada.
+
+Marcar una tarea como completada: Permitir al usuario marcar una tarea específica como completada. Esto implica cambiar el estado de la tarea de pendiente a completada.
+
+Eliminar una tarea: Permitir al usuario eliminar una tarea específica de la lista.
+
+Mostrar la lista de tareas: Mostrar todas las tareas en la lista enlazada junto con su estado (pendiente o completada).
+
+Implementación:
+
+Define la estructura TaskNode para representar cada nodo de la lista enlazada, que contenga campos para el nombre de la tarea, el estado y un puntero al siguiente nodo.
+
+Implementa funciones para cada una de las funcionalidades requeridas (agregar tarea, marcar como completada, eliminar tarea y mostrar lista).
+
+Este ejercicio te permitirá practicar cómo manipular listas enlazadas para almacenar y gestionar datos de manera dinámica, y es un buen punto de partida para entender las operaciones básicas sobre estructuras de datos enlazadas.
+*/
